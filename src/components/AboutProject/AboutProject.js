337decec6f84ef6221ead3a14bbdb98197f451c0
Evgenii Chernyshov <mailbox@eugesher.site>
2021-05-10 18:38:06 +0300

    feat: tab, mobile 'about project' section

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -6,16 +6,20 @@ export default function AboutProject() {
       <div className="section-content about-project__content">
         <h2 className="about-project__title">О проекте</h2>
         <div className="about-project__info-container">
-          <h3 className="about-project__info-title">Дипломный проект включал 5 этапов</h3>
-          <h3 className="about-project__info-title">На выполнение диплома ушло 5 недель</h3>
-          <p className="about-project__info-text">
-            Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные
-            доработки.
-          </p>
-          <p className="about-project__info-text">
-            У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы
-            успешно защититься.
-          </p>
+          <div className="about-project__info-column">
+            <h3 className="about-project__info-title">Дипломный проект включал 5 этапов</h3>
+            <p className="about-project__info-text">
+              Составление плана, работу над бэкендом, вёрстку, добавление функциональности и
+              финальные доработки.
+            </p>
+          </div>
+          <div className="about-project__info-column">
+            <h3 className="about-project__info-title">На выполнение диплома ушло 5 недель</h3>
+            <p className="about-project__info-text">
+              У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы
+              успешно защититься.
+            </p>
+          </div>
         </div>
         <div className="about-project__time-bar">
           <p className="about-project__time-bar-spent about-project__time-bar-spent_column_left">
