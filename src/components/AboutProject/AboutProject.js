62291fc2eec37af0ccfc3c75605f3fa92646852b
Evgenii Chernyshov <mailbox@eugesher.site>
2021-05-10 15:57:38 +0300

    feat: desktop 'about project' section

---
+++ src/components/AboutProject/AboutProject.js
@@ -0,0 +1,33 @@
+import "./AboutProject.css";
+
+export default function AboutProject() {
+  return (
+    <section className="about-project">
+      <div className="section-content about-project__content">
+        <h2 className="about-project__title">О проекте</h2>
+        <div className="about-project__info-container">
+          <h3 className="about-project__info-title">Дипломный проект включал 5 этапов</h3>
+          <h3 className="about-project__info-title">На выполнение диплома ушло 5 недель</h3>
+          <p className="about-project__info-text">
+            Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные
+            доработки.
+          </p>
+          <p className="about-project__info-text">
+            У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы
+            успешно защититься.
+          </p>
+        </div>
+        <div className="about-project__time-bar">
+          <p className="about-project__time-bar-spent about-project__time-bar-spent_column_left">
+            1 неделя
+          </p>
+          <p className="about-project__time-bar-spent about-project__time-bar-spent_column_right">
+            4 недели
+          </p>
+          <p className="about-project__time-bar-caption">Back-end</p>
+          <p className="about-project__time-bar-caption">Front-end</p>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,11 +1,12 @@
 import "./Main.css";
 import Promo from "../Promo/Promo";
+import AboutProject from "../AboutProject/AboutProject";
 
 export default function Main() {
   return (
     <main className="main">
       <Promo />
-      {/*<AboutProject />*/}
+      <AboutProject />
     </main>
   );
 }
