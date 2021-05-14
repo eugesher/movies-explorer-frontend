4d993e35f8679ebadb739bb4b7690adf29eca042
Evgenii Chernyshov <mailbox@eugesher.site>
2021-05-14 16:55:38 +0300

    feat: desktop 'footer' section

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
+import Footer from "../Footer/Footer";
 
 export default function App() {
   return (
     <div className="app">
       <Header />
       <Main />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,46 @@
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer__content">
+        <p className="footer__app-description">Учебный проект Яндекс.Практикум х BeatFilm.</p>
+        <div className="footer__main-container">
+          <p className="footer__copyright">&copy; 2020</p>
+          <ul className="footer__social-list">
+            <li>
+              <a
+                href="https://praktikum.yandex.ru/"
+                target="_blank"
+                rel="noreferrer"
+                className="footer__social-link"
+              >
+                Яндекс.Практикум
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://github.com/eugesher"
+                target="_blank"
+                rel="noreferrer"
+                className="footer__social-link"
+              >
+                GitHub
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/evgenii-chernyshov-3688201b6/"
+                target="_blank"
+                rel="noreferrer"
+                className="footer__social-link"
+              >
+                Facebook
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
+}
