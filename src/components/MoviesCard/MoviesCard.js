304ae4f1c79efd1b7a447b209364d7d2c919d906
Evgenii Chernyshov <mailbox@eugesher.site>
2021-05-20 17:42:32 +0300

    feat: tablet, mobile 'movies card list' section

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -5,7 +5,7 @@ import preview from "../../images/preview.png";
 export default function MoviesCard() {
   return (
     <div className="movies-card">
-      <div className="movies-card__column-container">
+      <div className="movies-card__main-container">
         <div className="movies-card__info-container">
           <h2 className="movies-card__title">33 слова о дизайне</h2>
           <p className="movies-card__duration">1ч 42м</p>
