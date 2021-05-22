16021a9126805ba7524e396507bf2d3ed7fdbda4
Evgenii Chernyshov <mailbox@eugesher.site>
2021-05-22 05:38:44 +0300

    feat: header logo link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,15 @@
 import "./Header.css";
 import logo from "../../images/logo.svg";
 import Navigation from "../Navigation/Navigation";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <header className="header">
       <div className="header__content">
-        <img src={logo} alt="логотип приложения" />
+        <Link to="/">
+          <img src={logo} alt="логотип приложения" />
+        </Link>
         <Navigation />
       </div>
     </header>
