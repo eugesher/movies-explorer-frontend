72ba4bbb3fc89c4051ef125001ee28172b0bed72
Evgenii Chernyshov <mailbox@eugesher.site>
2021-05-23 17:11:49 +0300

    feat: mobile menu handle overlay click, handle escape key

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -2,10 +2,32 @@ import "./MobileMenu.css";
 import { NavLink } from "react-router-dom";
 import ProfileLink from "../ProfileLink/ProfileLink";
 import crossIcon from "../../images/cross.svg";
+import { useEffect } from "react";
 
 export default function MobileMenu({ isOpen, onClose }) {
+  function handleOverlayClick(event) {
+    event.target.classList.contains("mobile-menu") && onClose();
+  }
+
+  useEffect(() => {
+    if (isOpen) {
+      const handleEscapeClose = (event) => {
+        if (event.key === "Escape") {
+          onClose();
+        }
+      };
+      document.addEventListener("keydown", handleEscapeClose);
+      return () => {
+        document.removeEventListener("keydown", handleEscapeClose);
+      };
+    }
+  }, [isOpen, onClose]);
+
   return (
-    <div className={`mobile-menu ${isOpen && "mobile-menu_opened"}`}>
+    <div
+      onMouseDown={handleOverlayClick}
+      className={`mobile-menu ${isOpen && "mobile-menu_opened"}`}
+    >
       <button type="button" onClick={onClose} className="mobile-menu__close-button">
         <img src={crossIcon} alt="закрыть" className="mobile-menu__close-icon" />
       </button>
