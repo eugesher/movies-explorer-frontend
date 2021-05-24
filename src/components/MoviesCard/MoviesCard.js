62e3ba21bab99a4d5ee4728544d50465adc16326
Evgenii Chernyshov <mailbox@eugesher.site>
2021-05-25 00:28:15 +0300

    fix: isLiked state

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,9 +1,17 @@
 import "./MoviesCard.css";
+import heart from "../../images/heart.svg";
 import heartSolid from "../../images/heart-solid.svg";
 import crossIcon from "../../images/cross.svg";
 import preview from "../../images/preview.png";
+import { useState } from "react";
 
 export default function MoviesCard({ isSaved }) {
+  const [isLiked, setIsLiked] = useState(false);
+
+  function handleLikeClick() {
+    isLiked ? setIsLiked(false) : setIsLiked(true);
+  }
+
   return (
     <div className="movies-card">
       <div className="movies-card__main-container">
@@ -11,9 +19,9 @@ export default function MoviesCard({ isSaved }) {
           <h2 className="movies-card__title">33 слова о дизайне</h2>
           <p className="movies-card__duration">1ч 42м</p>
         </div>
-        <button type="button" className="movies-card__action-button">
+        <button onClick={handleLikeClick} type="button" className="movies-card__action-button">
           <img
-            src={isSaved ? crossIcon : heartSolid}
+            src={isSaved ? crossIcon : isLiked ? heartSolid : heart}
             alt={isSaved ? "удалить фильм" : "сохранить фильм"}
             className="movies-card__action-button-image"
           />
