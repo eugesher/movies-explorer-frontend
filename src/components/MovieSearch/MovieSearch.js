77a913bf55fd04571a223d854de0d640b4436616
Evgenii Chernyshov <mailbox@eugesher.site>
2021-05-25 01:14:56 +0300

    fix: movie search input focus effect

--- src/components/MovieSearch/MovieSearch.js
+++ src/components/MovieSearch/MovieSearch.js
@@ -1,13 +1,26 @@
 import "./MovieSearch.css";
 import FilterCheckbox from "../FilterCheckbox/FilterCheckbox";
+import { useState } from "react";
 
 export default function MovieSearch() {
+  const [isFocused, setIsFocused] = useState(false);
+
   return (
     <section className="movie-search">
       <div className="movie-search__content">
         <form className="movie-search__form">
-          <div className="movie-search__input-group">
-            <input type="text" placeholder="Фильм" className="movie-search__input" />
+          <div
+            className={`movie-search__input-group ${
+              isFocused && "movie-search__input-group_focused"
+            }`}
+          >
+            <input
+              type="text"
+              placeholder="Фильм"
+              className="movie-search__input"
+              onFocus={() => setIsFocused(true)}
+              onBlur={() => setIsFocused(false)}
+            />
             <button type="submit" className="movie-search__submit-button">
               Поиск
             </button>
