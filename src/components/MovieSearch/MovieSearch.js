0f2c5396780d1435469298a59c38f54ef9ecc3e9
Evgenii Chernyshov <mailbox@eugesher.site>
2021-05-25 23:41:53 +0300

    feat: movie search

--- src/components/MovieSearch/MovieSearch.js
+++ src/components/MovieSearch/MovieSearch.js
@@ -2,13 +2,23 @@ import "./MovieSearch.css";
 import FilterCheckbox from "../FilterCheckbox/FilterCheckbox";
 import { useState } from "react";
 
-export default function MovieSearch() {
+export default function MovieSearch({ onMovieSearch }) {
   const [isFocused, setIsFocused] = useState(false);
+  const [formValue, setFormValue] = useState("");
+
+  function handleInputChange(event) {
+    setFormValue(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    onMovieSearch(formValue);
+  }
 
   return (
     <section className="movie-search">
       <div className="movie-search__content">
-        <form className="movie-search__form">
+        <form onSubmit={handleSubmit} className="movie-search__form">
           <div
             className={`movie-search__input-group ${
               isFocused && "movie-search__input-group_focused"
@@ -20,6 +30,7 @@ export default function MovieSearch() {
               className="movie-search__input"
               onFocus={() => setIsFocused(true)}
               onBlur={() => setIsFocused(false)}
+              onChange={handleInputChange}
               required
             />
             <button type="submit" className="movie-search__submit-button">
--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -13,6 +13,27 @@ export default function Movies({ windowWidth }) {
     setMoviesCount(moviesCount + addedMoviesCount);
   }
 
+  function handleMovieSearch(queryString) {
+    const matchedMovies = [];
+    moviesApi
+      .getMovies()
+      .then((data) => {
+        const re = new RegExp(queryString, "i");
+        data.forEach((movie) => {
+          if (re.test(movie.nameRU) || re.test(movie.nameEN)) {
+            matchedMovies.push(movie);
+          }
+        });
+        return matchedMovies;
+      })
+      .then((matchedMovies) => {
+        setMovies(matchedMovies);
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  }
+
   useEffect(() => {
     if (windowWidth >= 480) {
       setMoviesCount(6);
@@ -30,20 +51,20 @@ export default function Movies({ windowWidth }) {
     }
   }, [windowWidth]);
 
-  useEffect(() => {
-    moviesApi
-      .getMovies()
-      .then((data) => {
-        setMovies(data);
-      })
-      .catch((e) => {
-        console.error(e);
-      });
-  }, []);
+  // useEffect(() => {
+  //   moviesApi
+  //     .getMovies()
+  //     .then((data) => {
+  //       setMovies(data);
+  //     })
+  //     .catch((e) => {
+  //       console.error(e);
+  //     });
+  // }, []);
 
   return (
     <main className="movies">
-      <MovieSearch />
+      <MovieSearch onMovieSearch={handleMovieSearch} />
       <MoviesCardList isSaved={false} movies={movies.slice(0, moviesCount)} />
       <div className="movies__more-button-container">
         <button type="button" onClick={handleMoreButtonClick} className="movies__more-button">
